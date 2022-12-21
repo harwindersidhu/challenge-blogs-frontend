91a118b221bb3ff649d8442082c87792ca0d9860
Harwinder Singh Sidhu <harwindersidhu66@gmail.com>
2022-12-20 19:54:57 -0700

    Basic structure of app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-
+import Home from "./components/Home";
+import Blog from "./components/Blog";
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/">
+          <Route index element={<Home />} />
+          <Route path="blog" element={<Blog />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="blog">
+      Home
+    </div>
+  );
+}
+
+export default Home;
